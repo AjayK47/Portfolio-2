11311ac40d4cabb873893ab8fc67c4793e6bbaec
Ajay K <ajaykommineni8@gmail.com>
2024-12-27 18:40:04 +0530

    new updates

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -2,9 +2,9 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import { type ThemeProviderProps as NextThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
