63ad0b094cb7422966b35dd855b11a0f5e2e2f30
Ajay <88961945+AjayK47@users.noreply.github.com>
2025-01-28 14:54:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add Cloudflare Workers configuration
+  experimental: {
+    runtime: 'edge',
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
-
